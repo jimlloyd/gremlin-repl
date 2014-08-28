20f0c4251a0c4866a67fc68cd8d26bf7106cfa67
Jim Lloyd <jim.lloyd@gmail.com>
2014-08-28 15:32:48 -0700

    Add test/data/Examples.txt script, attempting to follow gremlin-node README.md examples.
    This is still work in progress. The goal is to provide working examples
    of all of the examples shown in https://github.com/inolen/gremlin-node/blob/master/README.md#examples

--- test/Transcript-test.js
+++ test/Transcript-test.js
@@ -10,7 +10,28 @@ var concat = require('concat-stream');
 
 describe('Transcript', function() {
 
-    it('a test script should produce an expected transcript', function(done) {
+    it('should produce the expected transcript given Examples.txt', function(done) {
+        this.timeout(5000);
+
+        var expected = fs.readFileSync('test/data/Examples.expected', { encoding: 'utf8' });
+        var expectedLines = expected.split('\n');
+
+        var output = concat({encoding: 'string'}, function(data) {
+            var dataLines = data.split('\n');
+            expect(dataLines).to.deep.equal(expectedLines);
+            done();
+        });
+
+        var options = {
+            prompt: 'node > ',
+            input: fs.createReadStream('test/data/Examples.txt'),
+            output: output,
+        };
+
+        repl.start(options);
+    });
+
+    it('should produce the expected transcript given test.txt', function(done) {
         this.timeout(5000);
 
         var expected = fs.readFileSync('test/data/test.expected', { encoding: 'utf8' });
