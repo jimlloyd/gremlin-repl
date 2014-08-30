46ad55f18caf456750e99d504e711b8bc6eca9f6
Jim Lloyd <jim.lloyd@gmail.com>
2014-08-29 17:57:44 -0700

    More Transcript tests, including gremlindocs-transform.

--- test/Transcript-test.js
+++ test/Transcript-test.js
@@ -8,48 +8,40 @@ var repl = require('../lib/repl-server.js');
 var fs = require('fs');
 var concat = require('concat-stream');
 
+function transcriptTest(name, done) {
+    var expected = fs.readFileSync('test/data/'+name+'.expected', { encoding: 'utf8' });
+    var expectedLines = expected.split('\n');
+
+    var output = concat({encoding: 'string'}, function(data) {
+        var dataLines = data.split('\n');
+        expect(dataLines).to.deep.equal(expectedLines);
+        done();
+    });
+
+    var options = {
+        prompt: 'node > ',
+        input: fs.createReadStream('test/data/'+name+'.txt'),
+        output: output,
+    };
+
+    repl.start(options);
+}
+
 describe('Transcript', function() {
 
     it('should produce the expected transcript given Examples.txt', function(done) {
         this.timeout(5000);
-
-        var expected = fs.readFileSync('test/data/Examples.expected', { encoding: 'utf8' });
-        var expectedLines = expected.split('\n');
-
-        var output = concat({encoding: 'string'}, function(data) {
-            var dataLines = data.split('\n');
-            expect(dataLines).to.deep.equal(expectedLines);
-            done();
-        });
-
-        var options = {
-            prompt: 'node > ',
-            input: fs.createReadStream('test/data/Examples.txt'),
-            output: output,
-        };
-
-        repl.start(options);
+        transcriptTest('Examples', done);
     });
 
     it('should produce the expected transcript given test.txt', function(done) {
         this.timeout(5000);
+        transcriptTest('test', done);
+    });
 
-        var expected = fs.readFileSync('test/data/test.expected', { encoding: 'utf8' });
-        var expectedLines = expected.split('\n');
-
-        var output = concat({encoding: 'string'}, function(data) {
-            var dataLines = data.split('\n');
-            expect(dataLines).to.deep.equal(expectedLines);
-            done();
-        });
-
-        var options = {
-            prompt: 'node > ',
-            input: fs.createReadStream('test/data/test.txt'),
-            output: output,
-        };
-
-        repl.start(options);
+    it('should produce the expected transcript given gremlindocs-transform.txt', function(done) {
+        this.timeout(5000);
+        transcriptTest('gremlindocs-transform', done);
     });
 
 });
