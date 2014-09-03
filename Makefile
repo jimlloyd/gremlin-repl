84ecde432b0982c192b0d283d55f53bd1a9a02bd
Jim Lloyd <jim.lloyd@gmail.com>
2014-09-03 15:02:28 -0700

    Various changes while working on gremlindocs methods tests. Most notably: Promises.
    This commit started as an attempt to implement test gremlindocs-methods test.
    But to test these methods it became clear that I needed to take advantage of promises.
    This spurred various changes, which in an ideal world would have been rebased into
    a more coherent set of commits, but I'm simply squashing all of the changes into
    this commit and moving on.

--- Makefile
+++ Makefile
@@ -6,4 +6,4 @@ lint:
 	find bin lib test -name "*.js" | xargs node_modules/jshint/bin/jshint
 
 test:
-	node_modules/mocha/bin/mocha --reporter=spec --ui tdd
+	DEBUG=repl-sever node_modules/mocha/bin/mocha --reporter=spec --ui tdd
--- test/Transcript-test.js
+++ test/Transcript-test.js
@@ -7,18 +7,17 @@ require('./support/config');
 var repl = require('../lib/repl-server.js');
 var fs = require('fs');
 var concat = require('concat-stream');
+var Q = require('q');
 
 function transcriptTest(name, done) {
     var expected = fs.readFileSync('test/data/'+name+'.expected', { encoding: 'utf8' });
     var expectedLines = expected.split('\n');
-    var semaphore = require('semaphore');
 
-    var sem = semaphore(0);
+    var concatDeferred = Q.defer();
 
     var output = concat({encoding: 'string'}, function(data) {
         var dataLines = data.split('\n');
-        expect(dataLines).to.deep.equal(expectedLines);
-        sem.leave();
+        concatDeferred.resolve(dataLines);
     });
 
     var options = {
@@ -29,14 +28,21 @@ function transcriptTest(name, done) {
 
     var replServer = repl.start(options);
 
-    replServer.endOfSessionPromise.done(function(){ sem.leave(); });
-
-    sem.take(2, function() {
-        done();
-    });
+    Q.all([concatDeferred.promise, replServer.endOfSessionPromise])
+        .then(function(results) {
+            var dataLines = results[0];
+            expect(dataLines).to.deep.equal(expectedLines);
+            done();
+        })
+        .done();
 }
 
 describe('Transcript', function() {
+
+    it('should produce the expected transcript given promises-test.txt', function(done) {
+        this.timeout(5000);
+        transcriptTest('promises-test', done);
+    });
 
     it('should produce the expected transcript given Examples.txt', function(done) {
         this.timeout(5000);
@@ -66,6 +72,11 @@ describe('Transcript', function() {
     it('should produce the expected transcript given gremlindocs-branch.txt', function(done) {
         this.timeout(5000);
         transcriptTest('gremlindocs-branch', done);
+    });
+
+    it('should produce the expected transcript given gremlindocs-methods.txt', function(done) {
+        this.timeout(10000);
+        transcriptTest('gremlindocs-methods', done);
     });
 
 });
